f6bb1143c50f6f24a0654aaefcb781c455cabff7
Philippe Vinchon <pvinchon@gilt.com>
2018-03-26 18:03:36 +0100

    Update tests

---
+++ test/AWS/Request.purs
@@ -0,0 +1,39 @@
+module Test.AWS.Request where
+
+import Prelude (Unit, bind, pure, unit, ($), (==))
+import Control.Monad.Aff (Aff, attempt, throwError)
+import Control.Monad.Eff.Class (liftEff)
+import Control.Monad.Eff.Exception (EXCEPTION, Error, error, message)
+import Data.Either (Either(..))
+import Data.Foreign (Foreign)
+
+import AWS.Service (ServiceName(..), service, defaultOptions)
+import AWS.Request (MethodName(..), request)
+import AWS.Request.Types (NoInput(..))
+
+main :: forall eff. Aff (exception :: EXCEPTION | eff) Unit
+main = do
+    _ <- testRequestUnknownMethod
+    _ <- testRequestMissingParameters
+
+    pure unit
+
+testRequestUnknownMethod :: forall eff. Aff (exception :: EXCEPTION | eff) Unit
+testRequestUnknownMethod = do
+    s3 <- liftEff $ service (ServiceName "S3") defaultOptions
+    errOrSuccess :: Either Error Foreign <- attempt $ request s3 (MethodName "unknown") (NoInput unit)
+    case errOrSuccess of
+        Right succ -> throwError $ error "AWS S3 method unknown shouldn't exist"
+        Left err -> if (message err) == "service[methodName] is not a function"
+            then pure unit
+            else throwError err
+
+testRequestMissingParameters :: forall eff. Aff (exception :: EXCEPTION | eff) Unit
+testRequestMissingParameters = do
+    s3 <- liftEff $ service (ServiceName "S3") defaultOptions
+    errOrSuccess :: Either Error Foreign <- attempt $ request s3 (MethodName "getBucketVersioning") (NoInput unit)
+    case errOrSuccess of
+        Right succ -> throwError $ error "AWS S3 getBucketVersioning should require parameters"
+        Left err -> if (message err) == "Missing required key 'Bucket' in params"
+            then pure unit
+            else throwError err
