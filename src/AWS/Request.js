c7e37c40a219913f373cd8bd845fe543d6b6e371
Philippe Vinchon <pvinchon@gilt.com>
2018-03-26 18:02:59 +0100

    Request takes a service to request from

--- src/AWS/Request.js
+++ src/AWS/Request.js
@@ -2,12 +2,11 @@
 
 const awsSdk = require("aws-sdk");
 
-exports.requestImpl = function(serviceName) {
+exports.requestImpl = function(service) {
   return function(methodName) {
     return function(params) {
       return function (onError, onSuccess) {
-        const awsService = new awsSdk[serviceName];
-        awsService[methodName](params, function(err, data) {
+        service[methodName](params, function(err, data) {
           if (err === null) onSuccess(data);
           else              onError(err);
         });
--- src/AWS/Request.purs
+++ src/AWS/Request.purs
@@ -1,8 +1,8 @@
-module AWS.Request ( ServiceName(..)
-                   , MethodName(..)
+module AWS.Request ( MethodName(..)
                    , request
                    ) where
 
+import Prelude
 import Control.Monad.Aff (Aff)
 import Control.Monad.Aff.Compat (EffFnAff, fromEffFnAff)
 import Control.Monad.Eff.Class (liftEff)
@@ -10,15 +10,14 @@ import Control.Monad.Eff.Exception (EXCEPTION)
 import Data.Foreign (Foreign)
 import Data.Foreign.Class (class Decode, class Encode, encode, decode)
 import F (liftF)
-import Prelude
-import Data.Maybe (Maybe(..))
 
-newtype ServiceName = ServiceName String
+import AWS.Service (Service(..))
+
 newtype MethodName = MethodName String
 
-foreign import requestImpl :: forall eff. String -> String -> Foreign -> EffFnAff (exception :: EXCEPTION | eff) Foreign
-request :: forall eff i o. Encode i => Decode o => ServiceName -> MethodName -> i -> Aff (exception :: EXCEPTION | eff) o
-request (ServiceName service) (MethodName method) i = do
+foreign import requestImpl :: forall eff. Foreign -> String -> Foreign -> EffFnAff (exception :: EXCEPTION | eff) Foreign
+request :: forall eff i o. Encode i => Decode o => Service -> MethodName -> i -> Aff (exception :: EXCEPTION | eff) o
+request (Service service) (MethodName method) i = do
     let fi = encode i
     fo <- requestImpl service method fi # fromEffFnAff
     decode fo # liftF # liftEff
