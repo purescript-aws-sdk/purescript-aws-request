8189a801af1091f9caa4da4ac91a1f0641a13e70
Philippe Vinchon <pvinchon@gilt.com>
2018-03-26 18:03:36 +0100

    Update tests

---
+++ test/AWS/Service.purs
@@ -0,0 +1,22 @@
+module Test.AWS.Service where
+
+import Prelude (Unit, bind, pure, unit, ($), (==))
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Exception (EXCEPTION, message, throw, throwException, try)
+import Data.Either (Either(..))
+
+import AWS.Service (ServiceName(..), defaultOptions, service)
+
+main :: forall eff. Eff (exception :: EXCEPTION | eff) Unit
+main = do
+    _ <- testUnknownService
+    pure unit
+
+testUnknownService :: forall eff. Eff (exception :: EXCEPTION | eff) Unit
+testUnknownService = do
+    errOrSuccess <- try $ service (ServiceName "unknown") defaultOptions
+    case errOrSuccess of
+        Right succ -> throw "AWS service unknown shouldn't exist"
+        Left err -> if (message err) == "awsSdk[serviceName] is not a constructor"
+            then pure unit
+            else throwException err
