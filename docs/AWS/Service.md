aeefddc1d1bddeed8c39266599f8dd33b0a98fca
Philippe Vinchon <pvinchon@gilt.com>
2018-03-27 11:02:59 +0100

    Use newtypes instead of records for options

--- docs/AWS/Service.md
+++ docs/AWS/Service.md
@@ -1,5 +1,137 @@
 ## Module AWS.Service
 
+#### `OptionsType`
+
+``` purescript
+type OptionsType = { params :: NullOrUndefined (StrMap String), endpoint :: NullOrUndefined String, accessKeyId :: NullOrUndefined String, secretAccessKey :: NullOrUndefined String, region :: NullOrUndefined String, maxRetries :: NullOrUndefined Int, maxRedirects :: NullOrUndefined Int, sslEnabled :: NullOrUndefined Boolean, paramValidation :: NullOrUndefined ParamValidation, computeChecksums :: NullOrUndefined Boolean, convertResponseTypes :: NullOrUndefined Boolean, correctClockSkew :: NullOrUndefined Boolean, s3ForcePathStyle :: NullOrUndefined Boolean, s3BucketEndpoint :: NullOrUndefined Boolean, s3DisableBodySigning :: NullOrUndefined Boolean, retryDelayOptions :: NullOrUndefined RetryDelayOptions, httpOptions :: NullOrUndefined HttpOptions, apiVersion :: NullOrUndefined String, apiVersions :: NullOrUndefined (StrMap String), systemClockOffset :: NullOrUndefined Int, signatureVersion :: NullOrUndefined String, signatureCache :: NullOrUndefined Boolean, dynamoDbCrc32 :: NullOrUndefined Boolean }
+```
+
+#### `Options`
+
+``` purescript
+newtype Options
+  = Options OptionsType
+```
+
+##### Instances
+``` purescript
+Newtype Options _
+Generic Options _
+Show Options
+Encode Options
+```
+
+#### `defaultOptions`
+
+``` purescript
+defaultOptions :: Options
+```
+
+#### `defaultOptions'`
+
+``` purescript
+defaultOptions' :: (OptionsType -> OptionsType) -> Options
+```
+
+#### `ParamValidationType`
+
+``` purescript
+type ParamValidationType = { min :: NullOrUndefined Boolean, max :: NullOrUndefined Boolean, pattern :: NullOrUndefined Boolean, enum :: NullOrUndefined Boolean }
+```
+
+#### `ParamValidation`
+
+``` purescript
+newtype ParamValidation
+  = ParamValidation ParamValidationType
+```
+
+##### Instances
+``` purescript
+Newtype ParamValidation _
+Generic ParamValidation _
+Show ParamValidation
+Encode ParamValidation
+```
+
+#### `defaultParamValidation`
+
+``` purescript
+defaultParamValidation :: ParamValidation
+```
+
+#### `defaultParamValidation'`
+
+``` purescript
+defaultParamValidation' :: (ParamValidationType -> ParamValidationType) -> ParamValidation
+```
+
+#### `RetryDelayOptionsType`
+
+``` purescript
+type RetryDelayOptionsType = { base :: NullOrUndefined Int }
+```
+
+#### `RetryDelayOptions`
+
+``` purescript
+newtype RetryDelayOptions
+  = RetryDelayOptions RetryDelayOptionsType
+```
+
+##### Instances
+``` purescript
+Newtype RetryDelayOptions _
+Generic RetryDelayOptions _
+Show RetryDelayOptions
+Encode RetryDelayOptions
+```
+
+#### `defaultRetryDelayOptions`
+
+``` purescript
+defaultRetryDelayOptions :: RetryDelayOptions
+```
+
+#### `defaultRetryDelayOptions'`
+
+``` purescript
+defaultRetryDelayOptions' :: (RetryDelayOptionsType -> RetryDelayOptionsType) -> RetryDelayOptions
+```
+
+#### `HttpOptionsType`
+
+``` purescript
+type HttpOptionsType = { proxy :: NullOrUndefined String, connectTimeout :: NullOrUndefined Int, timeout :: NullOrUndefined Int, xhrAsync :: NullOrUndefined Boolean, xhrWithCredentials :: NullOrUndefined Boolean }
+```
+
+#### `HttpOptions`
+
+``` purescript
+newtype HttpOptions
+  = HttpOptions HttpOptionsType
+```
+
+##### Instances
+``` purescript
+Newtype HttpOptions _
+Generic HttpOptions _
+Show HttpOptions
+Encode HttpOptions
+```
+
+#### `defaultHttpOptions`
+
+``` purescript
+defaultHttpOptions :: HttpOptions
+```
+
+#### `defaultHttpOptions'`
+
+``` purescript
+defaultHttpOptions' :: (HttpOptionsType -> HttpOptionsType) -> HttpOptions
+```
+
 #### `Service`
 
 ``` purescript
@@ -14,22 +146,16 @@ newtype ServiceName
   = ServiceName String
 ```
 
-#### `Options`
+#### `serviceImpl`
 
 ``` purescript
-type Options = { params :: NullOrUndefined (StrMap String), endpoint :: NullOrUndefined String, accessKeyId :: NullOrUndefined String, secretAccessKey :: NullOrUndefined String, region :: NullOrUndefined String, maxRetries :: NullOrUndefined Int, maxRedirects :: NullOrUndefined Int, sslEnabled :: NullOrUndefined Boolean, paramValidation :: NullOrUndefined (StrMap Boolean), computeChecksums :: NullOrUndefined Boolean, convertResponseTypes :: NullOrUndefined Boolean, correctClockSkew :: NullOrUndefined Boolean, s3ForcePathStyle :: NullOrUndefined Boolean, s3BucketEndpoint :: NullOrUndefined Boolean, s3DisableBodySigning :: NullOrUndefined Boolean, retryDelayOptions :: NullOrUndefined (StrMap Int), apiVersion :: NullOrUndefined String, apiVersions :: NullOrUndefined (StrMap String), systemClockOffset :: NullOrUndefined Int, signatureVersion :: NullOrUndefined String, signatureCache :: NullOrUndefined Boolean, dynamoDbCrc32 :: NullOrUndefined Boolean }
+serviceImpl :: forall eff. String -> Foreign -> Eff (exception :: EXCEPTION | eff) Foreign
 ```
 
 #### `service`
 
 ``` purescript
 service :: forall eff. ServiceName -> Options -> Eff (exception :: EXCEPTION | eff) Service
-```
-
-#### `defaultOptions`
-
-``` purescript
-defaultOptions :: Options
 ```
 
 
--- src/AWS/Service.purs
+++ src/AWS/Service.purs
@@ -1,9 +1,4 @@
-module AWS.Service ( Service(..)
-                   , ServiceName(..)
-                   , Options
-                   , service
-                   , defaultOptions
-                   ) where
+module AWS.Service where
 
 import Prelude (class Show, bind, pure, ($))
 import Control.Monad.Eff (Eff)
@@ -15,6 +10,7 @@ import Data.Foreign.NullOrUndefined (NullOrUndefined(..))
 import Data.Generic.Rep (class Generic)
 import Data.Generic.Rep.Show (genericShow)
 import Data.Maybe (Maybe(..))
+import Data.Newtype (class Newtype, over)
 import Data.StrMap (StrMap)
 
 -- params (map) — An optional map of parameters to bind to every request sent by this service object. For more information on bound parameters, see "Working with Services" in the Getting Started Guide.
@@ -28,27 +24,15 @@ import Data.StrMap (StrMap)
 -- maxRetries (Int) — the maximum amount of retries to attempt with a request. See AWS.ACM.maxRetries for more information.
 -- maxRedirects (Int) — the maximum amount of redirects to follow with a request. See AWS.ACM.maxRedirects for more information.
 -- sslEnabled (Boolean) — whether to enable SSL for requests.
--- paramValidation (Boolean|map) — whether input parameters should be validated against the operation description before sending the request. Defaults to true. Pass a map to enable any of the following specific validation features:
---   - min [Boolean] — Validates that a value meets the min constraint. This is enabled by default when paramValidation is set to true.
---   - max [Boolean] — Validates that a value meets the max constraint.
---   - pattern [Boolean] — Validates that a string value matches a regular expression.
---   - enum [Boolean] — Validates that a string value matches one of the allowable enum values.
+-- paramValidation (Boolean|map) — whether input parameters should be validated against the operation description before sending the request.
 -- computeChecksums (Boolean) — whether to compute checksums for payload bodies when the service accepts it (currently supported in S3 only)
 -- convertResponseTypes (Boolean) — whether types are converted when parsing response data. Currently only supported for JSON based services. Turning this off may improve performance on large response payloads. Defaults to true.
 -- correctClockSkew (Boolean) — whether to apply a clock skew correction and retry requests that fail because of an skewed client clock. Defaults to false.
 -- s3ForcePathStyle (Boolean) — whether to force path style URLs for S3 objects.
 -- s3BucketEndpoint (Boolean) — whether the provided endpoint addresses an individual bucket (false if it addresses the root API endpoint). Note that setting this configuration option requires an endpoint to be provided explicitly to the service constructor.
 -- s3DisableBodySigning (Boolean) — whether S3 body signing should be disabled when using signature version v4. Body signing can only be disabled when using https. Defaults to true.
--- retryDelayOptions (map) — A set of options to configure the retry delay on retryable errors. Currently supported options are:
---   - base [Int] — The base number of milliseconds to use in the exponential backoff for operation retries. Defaults to 100 ms for all services except DynamoDB, where it defaults to 50ms.
---   - customBackoff [function] — A custom function that accepts a retry count and returns the amount of time to delay in milliseconds. The base option will be ignored if this option is supplied.
--- httpOptions (map) — A set of options to pass to the low-level HTTP request. Currently supported options are:
---   - proxy [String] — the URL to proxy requests through
---   - agent [http.Agent, https.Agent] — the Agent object to perform HTTP requests with. Used for connection pooling. Defaults to the global agent (http.globalAgent) for non-SSL connections. Note that for SSL connections, a special Agent object is used in order to enable peer certificate verification. This feature is only available in the Node.js environment.
---   - connectTimeout [Int] — Sets the socket to timeout after failing to establish a connection with the server after connectTimeout milliseconds. This timeout has no effect once a socket connection has been established.
---   - timeout [Int] — Sets the socket to timeout after timeout milliseconds of inactivity on the socket. Defaults to two minutes (120000).
---   - xhrAsync [Boolean] — Whether the SDK will send asynchronous HTTP requests. Used in the browser environment only. Set to false to send requests synchronously. Defaults to true (async on).
---   - xhrWithCredentials [Boolean] — Sets the "withCredentials" property of an XMLHttpRequest object. Used in the browser environment only. Defaults to false.
+-- retryDelayOptions (map) — A set of options to configure the retry delay on retryable errors.
+-- httpOptions (map) — A set of options to pass to the low-level HTTP request.
 -- apiVersion (String, Date) — a String in YYYY-MM-DD format (or a date) that represents the latest possible API version that can be used in all services (unless overridden by apiVersions). Specify 'latest' to use the latest possible version.
 -- apiVersions (map<String, String|Date>) — a map of service identifiers (the lowercase service class name) with the API version to use when instantiating a service. Specify 'latest' for each individual that can use the latest available version.
 -- logger (#write, #log) — an object that responds to .write() (like a stream) or .log() (like the console object) in order to log information about requests
@@ -56,7 +40,7 @@ import Data.StrMap (StrMap)
 -- signatureVersion (String) — the signature version to sign requests with (overriding the API configuration). Possible values are: 'v2', 'v3', 'v4'.
 -- signatureCache (Boolean) — whether the signature to sign requests with (overriding the API configuration) is cached. Only applies to the signature version 'v4'. Defaults to true.
 -- dynamoDbCrc32 (Boolean) — whether to validate the CRC32 checksum of HTTP response bodies returned by DynamoDB. Default: true.
-type Options =
+type OptionsType =
     { params :: NullOrUndefined (StrMap String)
     , endpoint :: NullOrUndefined String
     , accessKeyId :: NullOrUndefined String
@@ -65,14 +49,15 @@ type Options =
     , maxRetries :: NullOrUndefined Int
     , maxRedirects :: NullOrUndefined Int
     , sslEnabled :: NullOrUndefined Boolean
-    , paramValidation :: NullOrUndefined (StrMap Boolean)
+    , paramValidation :: NullOrUndefined ParamValidation
     , computeChecksums :: NullOrUndefined Boolean
     , convertResponseTypes :: NullOrUndefined Boolean
     , correctClockSkew :: NullOrUndefined Boolean
     , s3ForcePathStyle :: NullOrUndefined Boolean
     , s3BucketEndpoint :: NullOrUndefined Boolean
     , s3DisableBodySigning :: NullOrUndefined Boolean
-    , retryDelayOptions :: NullOrUndefined (StrMap Int)
+    , retryDelayOptions :: NullOrUndefined RetryDelayOptions
+    , httpOptions :: NullOrUndefined HttpOptions
     , apiVersion :: NullOrUndefined String
     , apiVersions :: NullOrUndefined (StrMap String)
     , systemClockOffset :: NullOrUndefined Int
@@ -81,8 +66,14 @@ type Options =
     , dynamoDbCrc32 :: NullOrUndefined Boolean
     }
 
+newtype Options = Options OptionsType
+derive instance newtypeOptions :: Newtype Options _
+derive instance repGenericOptions :: Generic Options _
+instance showOptions :: Show Options where show = genericShow
+instance encodeOptions :: Encode Options where encode = Generic.genericEncode $ Generic.defaultOptions { unwrapSingleConstructors = true }
+
 defaultOptions :: Options
-defaultOptions =
+defaultOptions = Options
     { params: NullOrUndefined Nothing
     , endpoint: NullOrUndefined Nothing
     , accessKeyId: NullOrUndefined Nothing
@@ -99,6 +90,7 @@ defaultOptions =
     , s3BucketEndpoint: NullOrUndefined Nothing
     , s3DisableBodySigning: NullOrUndefined Nothing
     , retryDelayOptions: NullOrUndefined Nothing
+    , httpOptions: NullOrUndefined Nothing
     , apiVersion: NullOrUndefined Nothing
     , apiVersions: NullOrUndefined Nothing
     , systemClockOffset: NullOrUndefined Nothing
@@ -107,10 +99,91 @@ defaultOptions =
     , dynamoDbCrc32: NullOrUndefined Nothing
     }
 
-newtype TypedOptions = TypedOptions Options
-derive instance repGenericOptions :: Generic TypedOptions _
-instance showOptions :: Show TypedOptions where show = genericShow
-instance encodeOptions :: Encode TypedOptions where encode = Generic.genericEncode $ Generic.defaultOptions { unwrapSingleConstructors = true }
+defaultOptions' :: (OptionsType -> OptionsType) -> Options
+defaultOptions' f = over Options f defaultOptions
+
+-- Whether input parameters should be validated against the operation description before sending the request.
+--   - min [Boolean] — Validates that a value meets the min constraint. This is enabled by default when paramValidation is set to true.
+--   - max [Boolean] — Validates that a value meets the max constraint.
+--   - pattern [Boolean] — Validates that a string value matches a regular expression.
+--   - enum [Boolean] — Validates that a string value matches one of the allowable enum values.
+type ParamValidationType =
+    { min :: NullOrUndefined Boolean
+    , max :: NullOrUndefined Boolean
+    , pattern :: NullOrUndefined Boolean
+    , enum :: NullOrUndefined Boolean
+    }
+
+newtype ParamValidation = ParamValidation ParamValidationType
+derive instance newtypeParamValidation :: Newtype ParamValidation _
+derive instance repGenericParamValidation :: Generic ParamValidation _
+instance showParamValidation :: Show ParamValidation where show = genericShow
+instance encodeParamValidation :: Encode ParamValidation where encode = Generic.genericEncode $ Generic.defaultOptions { unwrapSingleConstructors = true }
+
+defaultParamValidation :: ParamValidation
+defaultParamValidation = ParamValidation
+    { min: NullOrUndefined Nothing
+    , max: NullOrUndefined Nothing
+    , pattern: NullOrUndefined Nothing
+    , enum: NullOrUndefined Nothing
+    }
+
+defaultParamValidation' :: (ParamValidationType -> ParamValidationType) -> ParamValidation
+defaultParamValidation' f = over ParamValidation f defaultParamValidation
+
+-- A set of options to configure the retry delay on retryable errors.
+--   - base [Integer] — The base number of milliseconds to use in the exponential backoff for operation retries. Defaults to 100 ms for all services except DynamoDB, where it defaults to 50ms.
+--   - customBackoff [function] — A custom function that accepts a retry count and returns the amount of time to delay in milliseconds. The base option will be ignored if this option is supplied.
+type RetryDelayOptionsType =
+    { base :: NullOrUndefined Int
+    }
+
+newtype RetryDelayOptions = RetryDelayOptions RetryDelayOptionsType
+derive instance newtypeRetryDelayOptions :: Newtype RetryDelayOptions _
+derive instance repGenericRetryDelayOptions :: Generic RetryDelayOptions _
+instance showRetryDelayOptions :: Show RetryDelayOptions where show = genericShow
+instance encodeRetryDelayOptions :: Encode RetryDelayOptions where encode = Generic.genericEncode $ Generic.defaultOptions { unwrapSingleConstructors = true }
+
+defaultRetryDelayOptions :: RetryDelayOptions
+defaultRetryDelayOptions = RetryDelayOptions
+    { base: NullOrUndefined Nothing
+    }
+
+defaultRetryDelayOptions' :: (RetryDelayOptionsType -> RetryDelayOptionsType) -> RetryDelayOptions
+defaultRetryDelayOptions' f = over RetryDelayOptions f defaultRetryDelayOptions
+
+-- A set of options to pass to the low-level HTTP request.
+--   - proxy [String] — the URL to proxy requests through
+--   - agent [http.Agent, https.Agent] — the Agent object to perform HTTP requests with. Used for connection pooling. Defaults to the global agent (http.globalAgent) for non-SSL connections. Note that for SSL connections, a special Agent object is used in order to enable peer certificate verification. This feature is only available in the Node.js environment.
+--   - connectTimeout [Int] — Sets the socket to timeout after failing to establish a connection with the server after connectTimeout milliseconds. This timeout has no effect once a socket connection has been established.
+--   - timeout [Int] — Sets the socket to timeout after timeout milliseconds of inactivity on the socket. Defaults to two minutes (120000).
+--   - xhrAsync [Boolean] — Whether the SDK will send asynchronous HTTP requests. Used in the browser environment only. Set to false to send requests synchronously. Defaults to true (async on).
+--   - xhrWithCredentials [Boolean] — Sets the "withCredentials" property of an XMLHttpRequest object. Used in the browser environment only. Defaults to false.
+type HttpOptionsType =
+    { proxy :: NullOrUndefined String
+    , connectTimeout :: NullOrUndefined Int
+    , timeout :: NullOrUndefined Int
+    , xhrAsync :: NullOrUndefined Boolean
+    , xhrWithCredentials :: NullOrUndefined Boolean
+    }
+
+newtype HttpOptions = HttpOptions HttpOptionsType
+derive instance newtypeHttpOptions :: Newtype HttpOptions _
+derive instance repGenericHttpOptions :: Generic HttpOptions _
+instance showHttpOptions :: Show HttpOptions where show = genericShow
+instance encodeHttpOptions :: Encode HttpOptions where encode = Generic.genericEncode $ Generic.defaultOptions { unwrapSingleConstructors = true }
+
+defaultHttpOptions :: HttpOptions
+defaultHttpOptions = HttpOptions
+    { proxy: NullOrUndefined Nothing
+    , connectTimeout: NullOrUndefined Nothing
+    , timeout: NullOrUndefined Nothing
+    , xhrAsync: NullOrUndefined Nothing
+    , xhrWithCredentials: NullOrUndefined Nothing
+    }
+
+defaultHttpOptions' :: (HttpOptionsType -> HttpOptionsType) -> HttpOptions
+defaultHttpOptions' f = over HttpOptions f defaultHttpOptions
 
 newtype Service = Service Foreign
 newtype ServiceName = ServiceName String
@@ -118,5 +191,5 @@ newtype ServiceName = ServiceName String
 foreign import serviceImpl :: forall eff. String -> Foreign -> Eff (exception :: EXCEPTION | eff) Foreign
 service :: forall eff. ServiceName -> Options -> Eff (exception :: EXCEPTION | eff) Service
 service (ServiceName name) options = do
-    f <- serviceImpl name $ encode $ TypedOptions options
+    f <- serviceImpl name $ encode options
     pure $ Service f
