9559d13a5f260bb38bbb12e8266324ef4ec37c00
Philippe Vinchon <pvinchon@gilt.com>
2018-04-26 10:30:02 +0100

    Remove enc/dec for unit

--- src/AWS/RequestTypes.purs
+++ src/AWS/RequestTypes.purs
@@ -14,18 +14,6 @@ instance showNoArguments :: Show NoArguments where show = genericShow
 instance decodeNoArguments :: Decode NoArguments where decode _ = pure $ NoArguments {}
 instance encodeNoArguments :: Encode NoArguments where encode _ = toForeign {}
 
-newtype NoOutput = NoOutput Unit
-derive instance repGenericNoOutput :: Generic NoOutput _
-instance showNoOutput :: Show NoOutput where show = genericShow
-instance decodeNoOutput :: Decode NoOutput where decode _ = pure $ NoOutput unit
-instance encodeNoOutput :: Encode NoOutput where encode _ = toForeign unit
-
-newtype NoInput = NoInput Unit
-derive instance repGenericNoInput :: Generic NoInput _
-instance showNoInput :: Show NoInput where show = genericShow
-instance decodeNoInput :: Decode NoInput where decode _ = pure $ NoInput unit
-instance encodeNoInput :: Encode NoInput where encode _ = toForeign unit
-
 newtype Timestamp = Timestamp Instant
 derive instance repGenericTimestamp :: Generic Timestamp _
 instance showTimestamp :: Show Timestamp where show = genericShow
