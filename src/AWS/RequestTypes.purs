856a20fd34fec9f1f1fa9844c81e19d7a649ad86
Jan Vincent Liwanag <jvliwanag@gmail.com>
2019-09-26 11:50:47 +0800

    Replace foreign-generic with simple-json
    * Depend on purescript-foreign and generics-rep directly instead

--- src/AWS/RequestTypes.purs
+++ src/AWS/RequestTypes.purs
@@ -6,16 +6,16 @@ import Data.DateTime.Instant (Instant)
 import Data.Generic.Rep (class Generic)
 import Data.Generic.Rep.Show (genericShow)
 import Foreign (unsafeToForeign, unsafeReadTagged)
-import Foreign.Class (class Decode, class Encode)
+import Simple.JSON (class ReadForeign, class WriteForeign)
 
 newtype NoArguments = NoArguments {}
 derive instance repGenericNoArguments :: Generic NoArguments _
 instance showNoArguments :: Show NoArguments where show = genericShow
-instance decodeNoArguments :: Decode NoArguments where decode _ = pure $ NoArguments {}
-instance encodeNoArguments :: Encode NoArguments where encode _ = unsafeToForeign {}
+instance readNoArguments :: ReadForeign NoArguments where readImpl _ = pure $ NoArguments {}
+instance writeNoArguments :: WriteForeign NoArguments where writeImpl _ = unsafeToForeign {}
 
 newtype Timestamp = Timestamp Instant
 derive instance repGenericTimestamp :: Generic Timestamp _
 instance showTimestamp :: Show Timestamp where show = genericShow
-instance decodeTimestamp :: Decode Timestamp where decode = unsafeReadTagged "Date"
-instance encodeTimestamp :: Encode Timestamp where encode = unsafeToForeign
+instance readTimestamp :: ReadForeign Timestamp where readImpl = unsafeReadTagged "Date"
+instance writeTimestamp :: WriteForeign Timestamp where writeImpl = unsafeToForeign
