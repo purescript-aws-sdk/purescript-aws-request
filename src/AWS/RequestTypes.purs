efaef72174abd45b0a9cc8feb7b449c59b30e223
Jan Vincent Liwanag <jvliwanag@gmail.com>
2020-11-19 15:50:45 +0800

    Remove NoArguments, Timestamp foreign instances

--- src/AWS/RequestTypes.purs
+++ src/AWS/RequestTypes.purs
@@ -1,21 +1,14 @@
-module AWS.Request.Types where
+module AWS.Request.Types
+       ( Timestamp(..)
+       ) where
 
 import Prelude
 
 import Data.DateTime.Instant (Instant)
-import Data.Generic.Rep (class Generic)
-import Data.Generic.Rep.Show (genericShow)
-import Foreign (unsafeToForeign, unsafeReadTagged)
-import Simple.JSON (class ReadForeign, class WriteForeign)
-
-newtype NoArguments = NoArguments {}
-derive instance repGenericNoArguments :: Generic NoArguments _
-instance showNoArguments :: Show NoArguments where show = genericShow
-instance readNoArguments :: ReadForeign NoArguments where readImpl _ = pure $ NoArguments {}
-instance writeNoArguments :: WriteForeign NoArguments where writeImpl _ = unsafeToForeign {}
+import Data.Newtype (class Newtype)
 
 newtype Timestamp = Timestamp Instant
-derive instance repGenericTimestamp :: Generic Timestamp _
-instance showTimestamp :: Show Timestamp where show = genericShow
-instance readTimestamp :: ReadForeign Timestamp where readImpl = unsafeReadTagged "Date"
-instance writeTimestamp :: WriteForeign Timestamp where writeImpl = unsafeToForeign
+
+derive instance timestampNewtype :: Newtype Timestamp _
+instance showTimestamp :: Show Timestamp where
+  show (Timestamp i) = "(Timestamp " <> show i <> ")"
